339ee2a88cf01130c429b37132271d8ea909e7bb
Coding Jerk <codingjerk@gmail.com>
2018-10-14 01:05:37 +0300

    [mindlna]: added

--- tools/install.sh
+++ tools/install.sh
@@ -10,5 +10,7 @@ chmod -w "${XDG_CONFIG_HOME}/htop/htoprc"
 
 sh "${XDG_CONFIG_HOME}/transmission/settings.json.in" > "${TRANSMISSION_HOME}/settings.json"
 
+sh "${XDG_CONFIG_HOME}/minidlna/minidlna.conf.in" > "${XDG_DATA_HOME}/minidlna/minidlna.conf"
+
 # === Void linux ===
 test -x '/usr/bin/xlocate' && xlocate -S
