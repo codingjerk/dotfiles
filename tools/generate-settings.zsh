8bc64ce686a8cba77f6ec84f3b7e800326d2a957
Coding Jerk <codingjerk@gmail.com>
2019-02-12 13:47:49 +0300

    [tools]: fix potential issue in generate-settings.zsh, update message for new wallpaper choosing mechanism

--- tools/generate-settings.zsh
+++ tools/generate-settings.zsh
@@ -2,7 +2,7 @@ bindkey -e
 ask() {
   TEMPVAR="$2"
   vared -p "$1: " -che TEMPVAR
-  echo $TEMPVAR
+  echo "$TEMPVAR"
 }
 
 cat <<EOF
@@ -12,5 +12,5 @@ export CJ_MONOFONT_SIZE=$(ask 'Font size' '12')
 export CJ_PRIMARY_MONITOR=$(ask 'Primary monitor' 'DRY')
 export CJ_SECONDARY_MONITOR=$(ask 'Secondary monitor' 'DIY')
 export CJ_X_AUTOSTART=$(ask 'Autostart X? (yes/no)' 'yes')
-export CJ_WALLPAPER='${CJ_DOTFILES}/assets/wallpaper-$(ask 'Wallpaper resolution' '1920x1080').png'
+export CJ_WALLPAPER='${CJ_DOTFILES}/assets/wallpaper-$(ask 'Wallpaper variant (mountain/custom)' 'mountain').png'
 EOF
