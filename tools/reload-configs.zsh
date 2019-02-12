ee2caf342b2fe97fab092fd62e633489404deeee
Coding Jerk <codingjerk@gmail.com>
2019-02-12 19:53:02 +0300

    [tools]: add termux-reload-settings call in reload-configs.zsh

--- tools/reload-configs.zsh
+++ tools/reload-configs.zsh
@@ -2,6 +2,8 @@ set -e -o pipefail
 
 . "${CJ_DOTFILES}/settings.sh"
 
+which termux-reload-settings > /dev/null && termux-reload-settings
+
 if [[ -v DISPLAY ]]; then
   touch "${XDG_CONFIG_HOME}/xfce4/terminal/terminalrc"
   i3-msg reload
