4f5a20c5ec24f626aefb3dc77c58cefd4bcf2804
Coding Jerk <codingjerk@gmail.com>
2019-02-14 19:28:27 +0300

    [motd]: format using pep8, add types, fix LVM storages

--- tools/motd.py
+++ tools/motd.py
@@ -1,53 +1,72 @@
+import collections
 import datetime
 import shutil
-import collections
+from typing import Any, Callable, List
 
-def term_length(line):
+Partition = collections.namedtuple("Partition", [
+    'device',
+    'mountpoint'
+])
+
+Meminfo = collections.namedtuple('Meminfo', [
+    'total',
+    'free',
+    'cached',
+    'used',
+])
+
+
+def term_length(line: str) -> int:
     action = 'COUNT'
     cursor = 0
     counter = 0
 
     for char in line:
-        if   action == 'COUNT' and char == '\u001B':
+        if action == 'COUNT' and char == '\u001B':
             action = 'SKIP'
-        elif action == 'SKIP'  and char == 'm':
+        elif action == 'SKIP' and char == 'm':
             action = 'COUNT'
         elif action == 'COUNT':
             counter += 1
 
     return counter
 
-def get_term_width():
+
+def get_term_width() -> int:
     return shutil.get_terminal_size((37, 0)).columns
 
-def print_with_cut(line):
+
+def print_with_cut(line: str) -> None:
     line_length = get_term_width()
 
     action = 'PRINT'
     counter = 0
     for char in line:
-        if   action == 'PRINT' and char == '\u001B':
+        if action == 'PRINT' and char == '\u001B':
             action = 'SKIP'
-        elif action == 'SKIP'  and char == 'm':
+        elif action == 'SKIP' and char == 'm':
             action = 'PRINT'
         elif action == 'PRINT':
             counter += 1
 
-            if counter > line_length: break
+            if counter > line_length:
+                break
 
         print(char, end='')
 
     print('')
 
-def print_center(*lines):
-    max_length  = max(term_length(line) for line in lines)
+
+def print_center(*lines: str) -> None:
+    max_length = max(term_length(line) for line in lines)
     line_length = get_term_width()
     margin_left = ' ' * round((line_length - max_length) / 2)
 
     for line in lines:
         print_with_cut(margin_left + line)
 
-def get_fillers_for_table(table):
+
+def get_fillers_for_table(table: List[List[str]]) -> List[List[str]]:
     table_rows = len(table)
     table_columns = len(table[0])
 
@@ -71,7 +90,8 @@ def get_fillers_for_table(table):
 
     return fillers
 
-def print_table(table, justifiers):
+
+def print_table(table: List[List[str]], justifiers: List[str]) -> None:
     fillers = get_fillers_for_table(table)
 
     lines = []
@@ -90,7 +110,8 @@ def print_table(table, justifiers):
 
     print_center(*lines)
 
-def show_banner():
+
+def show_banner() -> None:
     print_center(
         '\u001B[31m __ __ _____ __ __ _____ ___ _ _____ _____ ',
         '\u001B[31m|  |  |  ___|  |  |  ___|   | |_   _|     |',
@@ -121,12 +142,14 @@ def show_banner():
 
     print()
 
-def get_uptime():
+
+def get_uptime() -> datetime.timedelta:
     with open('/proc/uptime') as fh:
         seconds = float(fh.read().split()[0])
-        return datetime.timedelta(seconds = seconds)
+        return datetime.timedelta(seconds=seconds)
 
-def format_uptime(uptime):
+
+def format_uptime(uptime: datetime.timedelta) -> str:
     if uptime.days > 0:
         result = '%d days, ' % uptime.days
     else:
@@ -139,14 +162,16 @@ def format_uptime(uptime):
 
     return result
 
-def show_uptime():
+
+def show_uptime() -> None:
     uptime = format_uptime(get_uptime())
 
     print_center('\u001B[34mup: \u001B[36m' + uptime)
     print()
 
-def get_device_short_name(long_name):
-    if   '/dev/sd' in long_name:
+
+def get_device_short_name(long_name: str) -> str:
+    if '/dev/sd' in long_name:
         return long_name[-4:]
     elif '/dev/mapper' in long_name:
         return long_name.split('-')[-1]
@@ -155,23 +180,25 @@ def get_device_short_name(long_name):
     else:
         return long_name
 
-def get_color_by_usage(usage):
+
+def get_color_by_usage(usage: Meminfo) -> str:
     factor = usage.used / usage.total
     if factor > 0.8:
-        return '\u001B[31m' # red
+        return '\u001B[31m'  # red
     if factor > 0.7:
-        return '\u001B[35m' # orange
+        return '\u001B[35m'  # orange
     if factor > 0.6:
-        return '\u001B[33m' # yellow
+        return '\u001B[33m'  # yellow
     if factor > 0.5:
-        return '\u001B[36m' # magenta
+        return '\u001B[36m'  # magenta
     if factor > 0.4:
-        return '\u001B[34m' # blue
+        return '\u001B[34m'  # blue
 
-    return '\u001B[32m' # green
+    return '\u001B[32m'  # green
 
-def draw_bar(factor, color, length):
-    gray  = '\u001B[1;30m'
+
+def draw_bar(factor: float, color: str, length: int) -> str:
+    gray = '\u001B[1;30m'
     reset = '\u001B[0m'
 
     factor = max(0.0, min(1.0, factor))
@@ -180,20 +207,23 @@ def draw_bar(factor, color, length):
 
     return '[' + color + '=' * active + gray + '=' * inactive + reset + ']'
 
-def draw_resource_bar(factor):
+
+def draw_resource_bar(factor: float) -> str:
     color = get_color_by_load(factor)
     length = round(get_term_width() / 8 + 4)
 
     return draw_bar(factor, color, length)
 
-def draw_disk_bar(usage):
+
+def draw_disk_bar(usage: Any) -> str:
     factor = usage.used / usage.total
     color = get_color_by_usage(usage)
     length = round(get_term_width() / 8 + 4)
 
     return draw_bar(factor, color, length)
 
-def bytes_to_human(v):
+
+def bytes_to_human(v: float) -> str:
     units = ['b', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb']
     u = 0
     while v >= 1000:
@@ -202,7 +232,8 @@ def bytes_to_human(v):
 
     return "%.2f %s" % (v, units[u])
 
-def format_disk_usage(usage):
+
+def format_disk_usage(usage: Any) -> str:
     color = get_color_by_usage(usage)
     reset = '\u001B[0m'
 
@@ -211,7 +242,8 @@ def format_disk_usage(usage):
 
     return color + used + reset + ' / ' + total
 
-def partitions_to_table(partitions):
+
+def partitions_to_table(partitions: List[Partition]) -> List[List[str]]:
     table = []
     for partition in partitions:
         name = '\u001B[0m' + get_device_short_name(partition.device)
@@ -220,55 +252,71 @@ def partitions_to_table(partitions):
             bar = draw_disk_bar(raw_usage)
             usage = format_disk_usage(raw_usage)
         except PermissionError:
-            bar   = '\u001B[31m?\u001B[0m'
+            bar = '\u001B[31m?\u001B[0m'
             usage = '\u001B[31m?\u001B[0m'
 
         table.append([name, bar, usage])
 
     return table
 
-def get_mountpoints():
-    rawmounts = []
-    with open('/proc/mounts') as fh:
-      for line in fh.readlines():
-        if line.startswith("/dev/block") or line.startswith("/dev/sd"):
-          rawmounts.append(line)
 
-    Partition = collections.namedtuple("Partition", ['device', 'mountpoint'])
+def is_block_device_line(mount_line: str) -> bool:
+    block_prefixes = [
+        "/dev/block/",
+        "/dev/sd",
+        "/dev/mapper/",
+    ]
+
+    for prefix in block_prefixes:
+        if mount_line.startswith(prefix):
+            return True
+
+    return False
+
+
+def get_mountpoints() -> List[Partition]:
+    with open('/proc/mounts') as fh:
+        rawmounts = filter(is_block_device_line, fh.readlines())
+
     result = []
     for [device, mountpoint, *_] in map(str.split, rawmounts):
         result.append(Partition(device=device, mountpoint=mountpoint))
 
     return result
 
-def get_space_lines():
+
+def get_space_lines() -> List[List[str]]:
     partitions = get_mountpoints()
     return partitions_to_table(partitions)
 
-def get_color_by_load(load):
+
+def get_color_by_load(load: float) -> str:
     if load > 0.7:
-        return '\u001B[31m' # red
+        return '\u001B[31m'  # red
     if load > 0.6:
-        return '\u001B[35m' # orange
+        return '\u001B[35m'  # orange
     if load > 0.5:
-        return '\u001B[33m' # yellow
+        return '\u001B[33m'  # yellow
     if load > 0.4:
-        return '\u001B[36m' # magenta
+        return '\u001B[36m'  # magenta
     if load > 0.3:
-        return '\u001B[34m' # blue
+        return '\u001B[34m'  # blue
 
-    return '\u001B[32m' # green
+    return '\u001B[32m'  # green
 
-def get_loadavg():
+
+def get_loadavg() -> float:
     with open('/proc/loadavg', 'r') as fh:
         averages = fh.read().split()
         return float(averages[0])
 
-def get_cpu_count():
+
+def get_cpu_count() -> int:
     with open('/proc/cpuinfo') as fh:
         return fh.read().count("processor")
 
-def get_cpu_line():
+
+def get_cpu_line() -> List[str]:
     cpu_load = get_loadavg() / get_cpu_count()
     color = get_color_by_load(cpu_load)
 
@@ -277,10 +325,11 @@ def get_cpu_line():
 
     return ['cpu:', bar, val]
 
-def get_meminfo():
-    total = None
-    free = None
-    cached = None
+
+def get_meminfo() -> Meminfo:
+    total = 0
+    free = 0
+    cached = 0
     with open('/proc/meminfo') as fh:
         for [key, value, *unit] in map(str.split, fh.readlines()):
             if key == 'MemTotal:':
@@ -290,7 +339,6 @@ def get_meminfo():
             elif key == 'Cached:':
                 cached = int(value) * 1024
 
-    Meminfo = collections.namedtuple('Meminfo', ['total', 'free', 'cached', 'used'])
     return Meminfo(
         total=total,
         free=free,
@@ -298,17 +346,20 @@ def get_meminfo():
         used=total - free - cached
     )
 
-def get_mem_line(title, key):
+
+def get_mem_line(title: str, key: Callable[[Meminfo], float]) -> List[str]:
     meminfo = get_meminfo()
     mem_load = key(meminfo) / meminfo.total
     color = get_color_by_load(mem_load)
 
     bar = draw_resource_bar(mem_load)
-    val = color + bytes_to_human(key(meminfo)) + '\u001B[0m / ' + bytes_to_human(meminfo.total)
+    val = color + bytes_to_human(key(meminfo)) + \
+        '\u001B[0m / ' + bytes_to_human(meminfo.total)
 
     return [title + ':', bar, val]
 
-def show_resources():
+
+def show_resources() -> None:
     table = get_space_lines()
     table.append(['', '', ''])
     table.append(get_cpu_line())
@@ -318,8 +369,10 @@ def show_resources():
     print_table(table, ['RIGHT', 'LEFT', 'LEFT'])
     print()
 
-def fin():
+
+def fin() -> None:
     print('\u001B[0m', end='')
+
 
 show_banner()
 show_uptime()
