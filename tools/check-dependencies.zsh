6cb9f2345469ac340c319bd0819bcfc345a2a8c6
Coding Jerk <codingjerk@gmail.com>
2019-05-23 07:51:28 +0300

    [ranger]: remove ranger and it's configuration files, dependency and ignores

--- tools/check-dependencies.zsh
+++ tools/check-dependencies.zsh
@@ -40,7 +40,6 @@ if [[ "$mode" =~ opt ]]; then
   recommend binary "exa" exa
   recommend binary "hexyl" hexyl
   recommend binary "The Silver Searcher" ag
-  recommend binary "ranger" ranger
 fi
 
 # === Daemons ===
