085f2d9464540c41454d054b3fcc4f77451b926c
Coding Jerk <codingjerk@gmail.com>
2020-04-08 19:10:09 +0300

    [tools]: add ubuntu installation instructions

--- tools/one-command-install.sh
+++ tools/one-command-install.sh
@@ -44,6 +44,24 @@ EOF
 EOF
 }
 
+ubuntu_packages() {
+  sudo apt update
+  sudo apt install -y zsh neovim python3 python3-neovim curl less man-db tmux htop ssh gnupg2 pinentry-tty python3-pip pass
+
+  # Static binaries
+  curl "https://gitlab.com/codingjerk/dotfiles/uploads/61c9d40a257c6a5f68e64490e35c43b1/fzf" > "${HOME}/dotfiles/bin/fzf"
+  chmod +x "${HOME}/dotfiles/bin/fzf"
+
+  # Rust
+  cat >> "${HOME}/postinstall.sh" <<EOF
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  cargo install hexyl
+  cargo install ripgrep
+  cargo install exa
+  cargo install fd-find
+EOF
+}
+
 install_with() {
   common_install_pre
   "$1"
@@ -53,6 +71,8 @@ install_with() {
 
 if grep -q Microsoft /proc/version; then
   install_with wsl_packages
+elif grep -q Ubunty /proc/version; then
+  install_with ubuntu_packages
 else
   echo "Error: Unknown OS, can't install"
   exit 1
