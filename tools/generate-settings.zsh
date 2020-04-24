4eea01b75461d08cfd8ec673a4771d937eedcb70
Coding Jerk <codingjerk@gmail.com>
2020-04-24 23:07:37 +0300

    [wallpaper]: add railroad wallpaper

--- tools/generate-settings.zsh
+++ tools/generate-settings.zsh
@@ -14,5 +14,5 @@ export CJ_MONOFONT_SIZE=$(ask 'Font size' '12')
 export CJ_PRIMARY_MONITOR=$(ask 'Primary monitor' 'DRY')
 export CJ_SECONDARY_MONITOR=$(ask 'Secondary monitor' 'DIY')
 export CJ_X_AUTOSTART=$(ask 'Autostart X? (yes/no)' 'yes')
-export CJ_WALLPAPER='${CJ_DOTFILES}/assets/wallpaper-$(ask 'Wallpaper variant (mountain/custom)' 'mountain').png'
+export CJ_WALLPAPER='${CJ_DOTFILES}/assets/wallpaper-$(ask 'Wallpaper variant (railroad/mountain/mountain-2/custom)' 'railroad').png'
 EOF
