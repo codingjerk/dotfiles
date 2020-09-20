103fdae7751dd4cf995cf9fc0cec1a862c43b1fd
Coding Jerk <codingjerk@gmail.com>
2020-09-20 18:44:48 +0300

    [x11]: run xdg when reloading configs

--- tools/reload-configs.zsh
+++ tools/reload-configs.zsh
@@ -5,6 +5,7 @@ setopt ERR_EXIT NO_UNSET PIPE_FAIL
 which termux-reload-settings > /dev/null && termux-reload-settings
 
 if [[ -v DISPLAY ]]; then
+  xrdb -merge ${XDG_CONFIG_HOME}/urxvt/config
   i3-msg reload
   pkill -USR1 polybar
   feh --no-fehbg --bg-tile "${CJ_WALLPAPER}"
