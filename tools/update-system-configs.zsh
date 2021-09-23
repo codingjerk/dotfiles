1ed978d36405a4c6835f216f407b138c97fa076a
Denis Gruzdev <codingjerk@gmail.com>
2021-09-23 23:54:59 +0300

    feat: set default ttl to 65 to bypass modem restrictions

--- tools/update-system-configs.zsh
+++ tools/update-system-configs.zsh
@@ -55,6 +55,8 @@ net.core.netdev_max_backlog = 100000
 net.core.netdev_budget = 50000
 net.core.netdev_budget_usecs = 5000
 
+net.ipv4.ip_default_ttl = 65
+
 net.core.rmem_default = 1048576
 net.core.rmem_max = 16777216
 net.core.wmem_default = 1048576
