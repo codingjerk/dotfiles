743a790cdda42e7eb0bab64d6feef42ec8f15c42
Denis Gruzdev <codingjerk@gmail.com>
2022-09-12 21:32:35 +0000

    fix(tools): remove redundant reloading of i3wm configuration

--- tools/reload-configs.zsh
+++ tools/reload-configs.zsh
@@ -5,7 +5,6 @@ setopt ERR_EXIT NO_UNSET PIPE_FAIL
 which termux-reload-settings > /dev/null && termux-reload-settings
 
 if [[ -v DISPLAY ]]; then
-  i3-msg reload
   pkill -USR1 polybar
   feh --no-fehbg --bg-tile "${CJ_WALLPAPER}"
 fi
